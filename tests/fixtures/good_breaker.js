f01d5186c5fca25d8d8b4a1e4cae02f3c9b27b2d
Ilya Taratukhin <darkilfa@gmail.com>
2019-02-21 20:26:36 +0300

    update dependencies, fix linter errors, remove gulp-utils

--- tests/fixtures/good_breaker.js
+++ tests/fixtures/good_breaker.js
@@ -1,10 +1,10 @@
-const { warp } = require('../../lib/portal');
 const through = require('through2');
-const gutils = require('gulp-util');
+const PluginError = require('plugin-error');
+const { warp } = require('../../lib/portal');
 
 warp(sinkIn => sinkIn.pipe(through.obj((fl, tp, cb) => {
   const e = new Error('I\'m a breaker');
-  cb(new gutils.PluginError('good-breaker', e, {
+  cb(new PluginError('good-breaker', e, {
     fileName: fl.path,
     showStack: true,
   }));
